6651f6fe31f583d565b753502b1d85c332ce3c37
mrpupi <miromannz@mail.ru>
2024-02-15 21:14:17 +0300

    uijbuy

---
+++ js&ts/work.js
@@ -0,0 +1,13 @@
+console.log('hello,peep')
+
+// prompt('число', 500)
+
+
+
+// pz1. zadachka1. 
+// запросить число и возыести в степень "2"
+let num = parseInt(prompt('число ', 10 ))
+num = parseInt(prompt('число', 20))
+alert('число во 2 степени ' + ( num**2))
+
+// 
