3eb5b59d1db0bd6a765f669bbc1d294e3e323245
mrpupi <miromannz@mail.ru>
2024-02-16 01:10:07 +0300

    ngiudhiughd

--- js&ts/work.js
+++ js&ts/work.js
@@ -2,12 +2,36 @@ console.log('hello,peep')
 
 // prompt('число', 500)
 
+document.write('<div style="background-color: black; width: 200px; height: 200px; color: azure; margin: 50px auto;">ЭТО HTML С JAVASCRIPT</div>')
 
-
-// pz1. zadachka1. 
+// dz1. zadachka1. 
 // запросить число и возыести в степень "2"
-let num = parseInt(prompt('число ', 10 ))
-num = parseInt(prompt('число', 20))
-alert('число во 2 степени ' + ( num**2))
+// let num = parseInt(prompt('число ', 10 ))
+// num = parseInt(prompt('число', 20))
+// alert('число во 2 степени ' + ( num**2))
 
-// 
+// dz1. zadachka2. 
+// запросить у пользователя год рождения и вывести указав год как константу
+// const ageUser = prompt(' Укажите ваш Возраст')
+// const nameUser = prompt(' Укажите ваше Имя')
+// const surnameUser = prompt(' Укажите вашу Фамилию')
+
+// alert(`Всё верно? ${ageUser} ${nameUser} ${surnameUser}`)
+
+// dz1. zadachka3. 
+// длина стороны квадрата и вывести Р
+// const per = prompt('Укажите длину стороны квадрата')
+// const all = 4
+// alert(per*all)
+
+// dz1. zadachka4.
+// зная R вывести P 
+// const p = 3 
+// const r = prompt('Укажите радиус')
+// alert(2*p*r)
+
+// все остальное похоже друг на друга поэтому сделаю калькулятор и более менее сложные задачи 
+// и так приступим
+const firstNum = prompt('Первое число')
+const secondNum = prompt('Второе число')
+function()
